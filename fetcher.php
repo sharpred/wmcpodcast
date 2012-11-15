7303f4979251d4972580957882c25c84b7c3ea7a
Paul Ryan <paul@sharpred.com>
2012-11-15 09:35:32 +0000

    tidied up global variables

--- fetcher.php
+++ fetcher.php
@@ -3,181 +3,174 @@
 $host = 'http://www.wessexmp3.co.uk/wessex/';
 $file = 'recordings.php';
 $url = $host . $file;
+
 /**
- * @class rssFeed
- *
+ * @function createFeed
+ * converts an array of mp3 files data into an itunes podcast file
  */
-class rssFeed {
+function createFeed($feed) {
 	/**
-	 * @function createFeed
-	 * converts an array of mp3 files data into an itunes podcast file
+	 * ideas from
+	 * http://www.phpeveryday.com/articles/PHP-XML-Adding-XML-Nodes-P414.html
 	 */
-	function createFeed($feed) {
+	global $host, $file, $url;
+	// change $destination to another location if not running it on Paul's iMac :-)
+	$destination = '/Users/paulryanwork/Dropbox/Public/wmc_rehearsals.xml';
+	$xml = new DOMDocument();
+	$xml -> formatOutput = true;
+	/**
+	 * start to create the XML file
+	 */
+	$rss = $xml -> createElement('rss');
+	$rss -> setAttribute('xmlns:itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd/');
+	$rss -> setAttribute('version', '2.0');
+	$root = $xml -> appendChild($rss);
+	$channel = $xml -> createElement('channel');
+	$root -> appendChild($channel);
+
+	$item = $xml -> createElement("title");
+	$itemText = $xml -> createTextNode('Wessex Male Choir Rehearsal Recordings');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("link");
+	$itemText = $xml -> createTextNode($url);
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("language");
+	$itemText = $xml -> createTextNode('en-gb');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("copyright");
+	$itemText = $xml -> createTextNode('2012');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("itunes:subtitle");
+	$itemText = $xml -> createTextNode('Rehearsal Recordings');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("itunes:author");
+	$itemText = $xml -> createTextNode('Wessex Male Choir');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("itunes:summary");
+	$itemText = $xml -> createTextNode('WMC Rehearsal Recordings for Personal Choir Practice.');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("description");
+	$itemText = $xml -> createTextNode('Here are some MP3 recordings of the Wessex Male Choir rehearsals that you can download for your own personal use (practice makes perfect!).');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$owner = $xml -> createElement("itunes:owner");
+	$channel -> appendChild($owner);
+
+	$item = $xml -> createElement("itunes:name");
+	$itemText = $xml -> createTextNode('Wessex Male Choir');
+	$item -> appendChild($itemText);
+	$owner -> appendChild($item);
+
+	$item = $xml -> createElement("itunes:email");
+	$itemText = $xml -> createTextNode('membership@wessexmalechoir.co.uk');
+	$item -> appendChild($itemText);
+	$owner -> appendChild($item);
+
+	$item = $xml -> createElement("itunes:image");
+	$itemText = $xml -> createTextNode('http://www.wessexmalechoir.co.uk/images/logo2.jpg');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("itunes:category");
+	$itemText = $xml -> createTextNode('Education');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	$item = $xml -> createElement("itunes:category");
+	$itemText = $xml -> createTextNode('Music');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+
+	foreach ($feed as $entry) {
+		$url = $host . $entry['enclosure'];
+		$length = ltrim($entry['length']);
+		$length = str_replace(",", "", $length);
+		$length2 = substr($length, 0);
+
+		$longSummary = $entry['title'] . ' ' . $entry['summary'];
+		$item = $xml -> createElement("item");
+
+		$title = $xml -> createElement("title");
+		$titleText = $xml -> createTextNode($entry['title']);
+		$title -> appendChild($titleText);
+
+		$author = $xml -> createElement("itunes:author");
+		$authorText = $xml -> createTextNode('Wessex Mail Choir');
+		$author -> appendChild($authorText);
+
+		$subtitle = $xml -> createElement("itunes:subtitle");
+		$subTitleText = $xml -> createTextNode($entry['title']);
+		$subtitle -> appendChild($subTitleText);
+
+		$summary = $xml -> createElement("itunes:summary");
+		$summaryText = $xml -> createTextNode($longSummary);
+		$summary -> appendChild($summaryText);
+
+		$enclosure = $xml -> createElement("enclosure");
+		$enclosure -> setAttribute('url', $url);
+		$enclosure -> setAttribute('length', $length);
+		$enclosure -> setAttribute('type', "audio/mp3");
+
+		$pubDate = $xml -> createElement("pubDate");
+		$pubDateText = $xml -> createTextNode($entry['pubDate']);
+		$pubDate -> appendChild($pubDateText);
+
+		$duration = $xml -> createElement("itunes:duration");
+		$durationText = $xml -> createTextNode($entry['duration']);
+		$duration -> appendChild($durationText);
+
+		$keywords = $xml -> createElement("itunes:keywords");
+		$keywordsText = $xml -> createTextNode('Music, Training');
+		$keywords -> appendChild($keywordsText);
+
+		$guid = $xml -> createElement("guid");
+		$guidText = $xml -> createTextNode($url);
+		$guid -> appendChild($guidText);
+
+		$item -> appendChild($title);
+		$item -> appendChild($author);
+		$item -> appendChild($subtitle);
+		$item -> appendChild($summary);
+		$item -> appendChild($enclosure);
+		$item -> appendChild($pubDate);
+		$item -> appendChild($duration);
+		$item -> appendChild($keywords);
+		$item -> appendChild($guid);
+
 		/**
-		 * ideas from
-		 * http://www.phpeveryday.com/articles/PHP-XML-Adding-XML-Nodes-P414.html
+		 * add each item
 		 */
-		$host = 'http://www.wessexmp3.co.uk/wessex/';
-		$file = 'recordings.php';
-		$url = $host . $file;
-		$destination = '/Users/paulryanwork/Dropbox/Public/wmc_rehearsals.xml';
-		$xml = new DOMDocument();
-		$xml -> formatOutput = true;
-		/**
-		 * start to create the XML file
-		 */
-		$rss = $xml -> createElement('rss');
-		$rss -> setAttribute('xmlns:itunes', 'http://www.itunes.com/dtds/podcast-1.0.dtd/');
-		$rss -> setAttribute('version', '2.0');
-		$root = $xml -> appendChild($rss);
-		$channel = $xml -> createElement('channel');
-		$root -> appendChild($channel);
-
-		$item = $xml -> createElement("title");
-		$itemText = $xml -> createTextNode('Wessex Male Choir Rehearsal Recordings');
-		$item -> appendChild($itemText);
 		$channel -> appendChild($item);
 
-		$item = $xml -> createElement("link");
-		$itemText = $xml -> createTextNode($url);
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("language");
-		$itemText = $xml -> createTextNode('en-gb');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("copyright");
-		$itemText = $xml -> createTextNode('2012');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("itunes:subtitle");
-		$itemText = $xml -> createTextNode('Rehearsal Recordings');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("itunes:author");
-		$itemText = $xml -> createTextNode('Wessex Male Choir');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("itunes:summary");
-		$itemText = $xml -> createTextNode('WMC Rehearsal Recordings for Personal Choir Practice.');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("description");
-		$itemText = $xml -> createTextNode('Here are some MP3 recordings of the Wessex Male Choir rehearsals that you can download for your own personal use (practice makes perfect!).');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$owner = $xml -> createElement("itunes:owner");
-		$channel -> appendChild($owner);
-
-		$item = $xml -> createElement("itunes:name");
-		$itemText = $xml -> createTextNode('Wessex Male Choir');
-		$item -> appendChild($itemText);
-		$owner -> appendChild($item);
-
-		$item = $xml -> createElement("itunes:email");
-		$itemText = $xml -> createTextNode('membership@wessexmalechoir.co.uk');
-		$item -> appendChild($itemText);
-		$owner -> appendChild($item);
-
-		$item = $xml -> createElement("itunes:image");
-		$itemText = $xml -> createTextNode('http://www.wessexmalechoir.co.uk/images/logo2.jpg');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("itunes:category");
-		$itemText = $xml -> createTextNode('Education');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		$item = $xml -> createElement("itunes:category");
-		$itemText = $xml -> createTextNode('Music');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-
-		foreach ($feed as $entry) {
-			$url = $host . $entry['enclosure'];
-			$length = ltrim($entry['length']);
-			$length = str_replace(",", "", $length);
-			$length2 = substr($length, 0);
-
-			$longSummary = $entry['title'] . ' ' . $entry['summary'];
-			$item = $xml -> createElement("item");
-
-			$title = $xml -> createElement("title");
-			$titleText = $xml -> createTextNode($entry['title']);
-			$title -> appendChild($titleText);
-
-			$author = $xml -> createElement("itunes:author");
-			$authorText = $xml -> createTextNode('Wessex Mail Choir');
-			$author -> appendChild($authorText);
-
-			$subtitle = $xml -> createElement("itunes:subtitle");
-			$subTitleText = $xml -> createTextNode($entry['title']);
-			$subtitle -> appendChild($subTitleText);
-
-			$summary = $xml -> createElement("itunes:summary");
-			$summaryText = $xml -> createTextNode($longSummary);
-			$summary -> appendChild($summaryText);
-
-			$enclosure = $xml -> createElement("enclosure");
-			$enclosure -> setAttribute('url', $url);
-			$enclosure -> setAttribute('length', $length);
-			$enclosure -> setAttribute('type', "audio/mp3");
-
-			$pubDate = $xml -> createElement("pubDate");
-			$pubDateText = $xml -> createTextNode($entry['pubDate']);
-			$pubDate -> appendChild($pubDateText);
-
-			$duration = $xml -> createElement("itunes:duration");
-			$durationText = $xml -> createTextNode($entry['duration']);
-			$duration -> appendChild($durationText);
-
-			$keywords = $xml -> createElement("itunes:keywords");
-			$keywordsText = $xml -> createTextNode('Music, Training');
-			$keywords -> appendChild($keywordsText);
-
-			$guid = $xml -> createElement("guid");
-			$guidText = $xml -> createTextNode($url);
-			$guid -> appendChild($guidText);
-
-			$item -> appendChild($title);
-			$item -> appendChild($author);
-			$item -> appendChild($subtitle);
-			$item -> appendChild($summary);
-			$item -> appendChild($enclosure);
-			$item -> appendChild($pubDate);
-			$item -> appendChild($duration);
-			$item -> appendChild($keywords);
-			$item -> appendChild($guid);
-
-			/**
-			 * add each item
-			 */
-			$channel -> appendChild($item);
-
-		}
-		$explicit = $xml -> createElement('itunes:explicit');
-		$explicitText = $xml -> createTextNode('no');
-		$explicit -> appendChild($explicitText);
-		$channel -> appendChild($explicit);
-
-		$item = $xml -> createElement("itunes:block");
-		$itemText = $xml -> createTextNode('no');
-		$item -> appendChild($itemText);
-		$channel -> appendChild($item);
-		/**
-		 * file creation finished, now save it to disk
-		 */
-		$xml -> save($destination);
 	}
+	$explicit = $xml -> createElement('itunes:explicit');
+	$explicitText = $xml -> createTextNode('no');
+	$explicit -> appendChild($explicitText);
+	$channel -> appendChild($explicit);
 
+	$item = $xml -> createElement("itunes:block");
+	$itemText = $xml -> createTextNode('no');
+	$item -> appendChild($itemText);
+	$channel -> appendChild($item);
+	/**
+	 * file creation finished, now save it to disk
+	 */
+	$xml -> save($destination);
 }
 
 /**
@@ -194,17 +187,16 @@ function fetchPage($url) {
 	curl_close($ch);
 	return $html;
 }
+
 /**
  * @function main
  * loads the table of mp3's, strips unwanted details from the table and creates a URL of
  * mp3s
- * passes mp3 array to rss.createFeed($songs) to create the podcast XML and save it in 
+ * passes mp3 array to rss.createFeed($songs) to create the podcast XML and save it in
  * a local (public facing) Dropbox file
  */
 function main() {
-	$host = 'http://www.wessexmp3.co.uk/wessex/';
-	$file = 'recordings.php';
-	$url = $host . $file;
+	global $host, $file, $url;
 	$str = fetchPage($url);
 	//strip non breaking space from output (two attempts!)
 	$newstr = trim($str, chr(0xC2) . chr(0xA0));
@@ -246,8 +238,7 @@ function main() {
 	}
 	// don't want the title
 	array_shift($songs);
-	$feed = new rssFeed;
-	$feed -> createFeed($songs);
+	createFeed($songs);
 }
 
 main();
